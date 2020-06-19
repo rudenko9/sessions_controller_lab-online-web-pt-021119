e4c7dd4fa951dd9619109e65431553de9e3682b1
Nataliya R <n.rudenko@gmail.com>
2020-06-19 03:14:15 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
   end
   
   def create
-    @name = Name.new(name_params)
+    @name = Name.new
     @name.save
     redirect_to sessions_new
   end
