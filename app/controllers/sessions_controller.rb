396374fe06eff2076f83b6b18641574b4abc0658
Nataliya R <n.rudenko@gmail.com>
2020-06-19 03:17:36 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,11 @@ class SessionsController < ApplicationController
   def new
     
     @name = Name.all
+    if params[:name_id] && !Name.exists?(params[:name_id])
+      redirect_to name_path, alert: "Name not found."
+    else
+      @name = Name.new(name_id: params[:name_id])
+    end
     
     
   end
